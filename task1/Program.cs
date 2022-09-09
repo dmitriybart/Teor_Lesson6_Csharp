66b5780ce3997d84afdf1f185abee60a4c180a12
Dmitriy B <kupperfeet@gmail.com>
2022-09-09 18:46:25 +0300

    first commit2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Масштабируем фигуру через строки
+
+using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (6,7)";
+            .Replace("(","");
+            .Replace("(","");
+
+var data = text.Split(" ")
+
+            .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
